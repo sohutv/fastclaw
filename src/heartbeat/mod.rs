e7752913506df5cac67be0c7ab039afa940168aa
wenhaozhao <wenhaozhao@sohu-inc.com>
2026-04-02 16:48:48 +0800

    Refactor `Heartbeat` module: move `spawn_cron_tasks` to a separate file for better modularity and code organization.

--- src/heartbeat/mod.rs
+++ src/heartbeat/mod.rs
@@ -1,17 +1,13 @@
 use crate::agent::{Agent, AgentRequest, HistoryManager, LlmAgentSupplier};
-use crate::channels::{Anonymous, SessionId};
 use crate::config::{Config, Workspace};
 use crate::model_provider::ModelProviders;
-use crate::tools::TaskTools;
-use chrono::TimeDelta;
-use log::{error, info};
-use rig::completion::Message;
-use std::str::FromStr;
+use log::error;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::RwLock;
 use tokio::sync::mpsc::Sender;
 use tokio::task::JoinHandle;
+mod spawn_cron_tasks;
 
 #[allow(unused)]
 pub struct Heartbeat {
@@ -72,82 +68,5 @@ impl Heartbeat {
             }
         });
         Ok(handle)
-    }
-}
-
-impl Heartbeat {
-    async fn spawn_cron_tasks(
-        _: &'static Config,
-        workspace: &Workspace,
-        interval: Duration,
-        agent_message_sender: Sender<AgentRequest>,
-    ) -> crate::Result<()> {
-        let tasks = TaskTools::fetch_ready_tasks(workspace).await?;
-        let now = chrono::Local::now();
-        let execute_deadline = TimeDelta::from_std(interval)
-            .map(|delta| now + delta)
-            .unwrap_or(now);
-        for task in tasks {
-            let cron = task.cron;
-            // Parse cron expression and check if current time matches
-            match cron::Schedule::from_str(&cron) {
-                Ok(schedule) => {
-                    let last_exe_at = task.last_exe_at.unwrap_or(task.created_at);
-                    if let Some(next) = schedule.after(&last_exe_at).next() {
-                        if next <= execute_deadline {
-                            if let Err(err) =
-                                TaskTools::mark_task_executed_at(workspace, task.id, next).await
-                            {
-                                error!(
-                                    "Failed to update last_exe_at for task '{}' (id: {}): {}",
-                                    task.name, task.id, err
-                                );
-                                continue;
-                            }
-                            let session_id = SessionId::Anonymous {
-                                val: Anonymous(task.session_id.clone()),
-                                settings: Default::default(),
-                            };
-                            let agent_message_sender = agent_message_sender.clone();
-                            let _ = tokio::spawn(async move {
-                                match agent_message_sender
-                                    .send(AgentRequest {
-                                        session_id: session_id,
-                                        message: Message::user(format!(
-                                            r#"
-**Execute task immediately**: task_id: {}
-**Tips**: If the task fails, you are authorized to retry or ignore it at your discretion.
-                                            "#,
-                                            task.id
-                                        )),
-                                    })
-                                    .await
-                                {
-                                    Ok(_) => {
-                                        info!(
-                                            "Task '{}' (id: {}) is ready to execute based on cron schedule: {}, next trigger: {}",
-                                            task.name, task.id, cron, next
-                                        );
-                                    }
-                                    Err(err) => {
-                                        error!(
-                                            "Failed to send agent request for task '{}': {}",
-                                            task.name, err
-                                        );
-                                    }
-                                }
-                            });
-                        }
-                    }
-                }
-                Err(e) => {
-                    error!(
-                        "Failed to parse cron expression '{}' for task {}: {}",
-                        cron, task.id, e
-                    );
-                }
-            }
-        }
-        Ok(())
     }
 }
